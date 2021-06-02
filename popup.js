06737bfe87fab6169fa8a97111f919a35986bc2d
Elijah Delos Reyes <elijah@Elijahs-MBP.lan>
2021-06-02 12:15:27 -0700

    send data to server

--- popup.js
+++ popup.js
@@ -1,36 +1,49 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  // isLoggedIn();
-  setForm();
-  displayUrl(tabs[0].url);
+  getUserInfo();
+  displayForm(tabs[0].url);
 });
 
-function isLoggedIn() {
-  // GET request to server
+function getUserInfo() {
+  addEventListener(
+    "load",
+    function () {
+      var MAJOR_VERSION = 1.0;
+      if (
+        !localStorage.updateread ||
+        localStorage.updateread != MAJOR_VERSION
+      ) {
+        var email = prompt("What is your Google email address?");
+        localStorage["Email"] = email;
+        localStorage.updateread = MAJOR_VERSION;
+      }
+    },
+    0
+  );
 }
 
-function setForm() {
-  button = document.getElementById("diffuse-button");
+function displayForm(url) {
+  strippedUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split("/")[0];
+  document.getElementById("url").innerHTML = strippedUrl;
 
+  button = document.getElementById("diffuse-button");
   button.addEventListener("click", function (e) {
     title = document.getElementById("title").value;
     brainfart = document.getElementById("brainfart").value;
-    categories = document.getElementById("categories").value;
+    category = document.getElementById("categories").value;
 
-    alert(title + " " + brainfart + " " + categories);
+    sendData(title, brainfart, category);
   });
 }
 
-function displayUrl(url) {
-  strippedUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split("/")[0];
-  document.getElementById("url").innerHTML = strippedUrl;
-}
-
-function load() {
-  var xhttp = new XMLHttpRequest();
-
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-  };
+function sendData(title, brainfart, category) {
+  fetch("http://localhost:3000/api/getLists", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ a: 1, b: "Textual content" }),
+  })
+    .then((res) => res.json())
+    .then((res) => alert(res));
 }
