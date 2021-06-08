099e70f820220a923fb7765b5ad248ff6023f854
Elijah Delos Reyes <delosreyes.elijah@gmail.com>
2021-06-07 22:42:07 -0700

    just cors err

--- popup.js
+++ popup.js
@@ -1,49 +1,65 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  getUserInfo();
-  displayForm(tabs[0].url);
+	getUserInfo();
+	displayForm(tabs[0].url);
 });
 
 function getUserInfo() {
-  addEventListener(
-    "load",
-    function () {
-      var MAJOR_VERSION = 1.0;
-      if (
-        !localStorage.updateread ||
-        localStorage.updateread != MAJOR_VERSION
-      ) {
-        var email = prompt("What is your Google email address?");
-        localStorage["Email"] = email;
-        localStorage.updateread = MAJOR_VERSION;
-      }
-    },
-    0
-  );
+	addEventListener(
+		'load',
+		function () {
+			var MAJOR_VERSION = 1.0;
+			if (
+				!localStorage.updateread ||
+				localStorage.updateread != MAJOR_VERSION
+			) {
+				var email = prompt('What is your Google email address?');
+				localStorage['Email'] = email;
+				localStorage.updateread = MAJOR_VERSION;
+			}
+		},
+		0
+	);
 }
 
 function displayForm(url) {
-  strippedUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split("/")[0];
-  document.getElementById("url").innerHTML = strippedUrl;
+	strippedUrl = url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '').split('/')[0];
+	document.getElementById('url').innerHTML = strippedUrl;
 
-  button = document.getElementById("diffuse-button");
-  button.addEventListener("click", function (e) {
-    title = document.getElementById("title").value;
-    brainfart = document.getElementById("brainfart").value;
-    category = document.getElementById("categories").value;
+	button = document.getElementById('diffuse-button');
+	button.addEventListener('click', function (e) {
+		title = document.getElementById('title').value;
+		brainfart = document.getElementById('brainfart').value;
+		category = document.getElementById('categories').value;
 
-    sendData(title, brainfart, category);
-  });
+		sendData(title, brainfart, category);
+		// getData();
+	});
 }
 
 function sendData(title, brainfart, category) {
-  fetch("http://localhost:3000/api/getLists", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ a: 1, b: "Textual content" }),
-  })
-    .then((res) => res.json())
-    .then((res) => alert(res));
+	url =
+		'https://diffusion-web-app-mvp-default-rtdb.firebaseio.com/' +
+		localStorage['Email'] +
+		'/newList';
+
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'POST, GET, PUT',
+			'Access-Control-Allow-Headers':
+				'Origin, X-Requested-With, Content-Type, Accept',
+		},
+		body: JSON.stringify({ a: 1, b: 'Textual content' }),
+	})
+		.then((res) => res.json())
+		.then((res) => alert(res));
+}
+
+function getData() {
+	fetch('https://diffusion-web-app-mvp-default-rtdb.firebaseio.com/data.json')
+		.then((response) => response.json())
+		.then((data) => console.log(data));
 }
